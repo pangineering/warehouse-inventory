35fcb913c540774f70bdd71ddf5d98c728e68c74
pantkdkrt <p.engineering6415@gmail.com>
2022-07-06 21:19:05 +0700

    customer info

--- inventoryApp/customers/views.py
+++ inventoryApp/customers/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from .form import CustomerForm
 from .models import Customers
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def customer_list(request):
     customers = Customers.objects.all()
     context = {'customers':customers}
     return render(request, 'customers.html', context)
 
+@login_required
 def customer_add(request):
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
@@ -25,5 +28,12 @@ def customer_add(request):
         form = CustomerForm()
     return render(request, 'customer_add.html', {'form': form})
 
+@login_required
 def customer_edit(request):
     return render(request, 'customer_edit.html', {})
+
+@login_required
+def customer_info(request,pk):
+    customer = Customers.objects.get(pk=pk)
+    context = {'customer':customer}
+    return render(request, 'customer_info.html', context)
