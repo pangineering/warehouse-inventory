acd43faf77bb1ce7a35557a3304fef59f2c59b56
pantkdkrt <p.engineering6415@gmail.com>
2022-07-07 15:24:28 +0700

    fix Supplier and Customer form

--- inventoryApp/suppliers/form.py
+++ inventoryApp/suppliers/form.py
@@ -2,12 +2,11 @@ from django import forms
 from .models import Suppliers
 
 class SupplierForm(forms.ModelForm):
-    number = forms.CharField(label='Supplier number', max_length=100)
-    name = forms.CharField(label='Supplier name', max_length=100)
+    number = forms.CharField(label='Supplier Number', max_length=100)
+    name = forms.CharField(label='Supplier Name', max_length=100)
     company_name = forms.CharField(label='Supplier Company Name', max_length=100)
     email = forms.CharField(label='Supplier Email', max_length=100)
     mobile = forms.CharField(label='Supplier Mobile', max_length=100)
-    status = forms.CharField(label='Supplier Status', max_length=100)
 
     class Meta:
         model = Suppliers
